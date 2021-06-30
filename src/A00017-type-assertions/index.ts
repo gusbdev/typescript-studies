7f84e0cdb8fa753d40c6bcd90641fff54b980042
Gus <gusbdev@gmail.com>
2021-06-30 18:47:43 -0300

    Lesson 17

---
+++ src/A00017-type-assertions/index.ts
@@ -0,0 +1,27 @@
+/**
+ * Recomend
+ */
+
+// Conditional
+const body1 = document.querySelector('body');
+if (body1) body1.style.background = 'red';
+
+// Type assetion
+const body3 = document.querySelector('body') as HTMLBodyElement;
+body3.style.background = 'red';
+
+// HTMLElement
+const input = document.querySelector('.input') as HTMLInputElement;
+input.value = 'Anything';
+input.focus();
+
+/**
+ * Not recommended
+ */
+
+// Non-null assetion (!)
+const body2 = document.querySelector('body')!;
+body2.style.background = 'red';
+
+// Type assetion
+const body4 = document.querySelector('body') as unknown as number;
