9afb986639cdf27a7416de974e40da02631ece41
Gus <gusbdev@gmail.com>
2021-06-30 17:39:04 -0300

    Lesson 12

---
+++ src/A00012-type-literals/index.ts
@@ -0,0 +1,17 @@
+let x = 10; // eslint-disable-line
+x = 0b1010;
+const y = 10;
+let a = 100 as const; // eslint-disable-line
+
+const person = {
+  name: 'Gus' as const,
+  lastName: 'Bar',
+};
+
+function escolhaCor(cor: 'Vermelho' | 'Amarelo' | 'Azul'): string {
+  return cor;
+}
+
+console.log(escolhaCor('Vermelho'));
+
+export default 1;
