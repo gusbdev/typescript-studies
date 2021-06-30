ab1628d1668c8ddbe2cec3ad29c5bcef97b7addb
Gus <gusbdev@gmail.com>
2021-06-30 18:00:08 -0300

    Lesson 14

---
+++ src/A00014-intersection-types/index.ts
@@ -0,0 +1,20 @@
+type TemNome = { nome: string };
+type TemSobrenome = { sobrenome: string };
+type TemIdade = { idade: number };
+type Pessoa = TemNome & TemSobrenome & TemIdade; // AND
+
+type AB = 'A' | 'B';
+type AC = 'A' | 'C';
+type AD = 'D' | 'A';
+type Intersecao = AB & AC & AD;
+
+const pessoa: Pessoa = {
+  idade: 26,
+  nome: 'Gus',
+  sobrenome: 'Bar',
+};
+
+console.log(pessoa);
+
+// Module mode
+export { pessoa };
